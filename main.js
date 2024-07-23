858dd3b31b86f657e20cc7b60053b8a227e47c56
abhishekcoder00 <jangidabhi1008@gmail.com>
2024-07-23 05:03:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+    function opensearch() {
+        document.getElementById("myOverlay").style.display = "block";
+      }
+      
+      function closesearch() {
+        document.getElementById("myOverlay").style.display = "none";
+      }
+      function toggle(){
+        document.getElementsByTagName("body").style.width ="100%";
+      } 
+
